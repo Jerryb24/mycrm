642991cb8526541ddfae9ec597524a73a2cb8c36
Jerryb24 <Jerryndhine24@gmail.com>
2025-06-17 19:00:28 +0000

    Add login page

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Willkommen zu meinem CRM ✨</h1>
-      <p>Diese Seite ist der Anfang meiner eigenen Softwarelösung.</p>
-    </main>
-  );
-}
